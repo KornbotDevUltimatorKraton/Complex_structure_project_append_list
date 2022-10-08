a77ec0c2f0abfe420cdd0cc9ad17bc677178036d
Chanapai Chuadchum <kornbot359@hotmail.com>
2022-10-08 14:40:54 +0700

    Add files via upload

---
+++ Append_project_list_data.py
@@ -0,0 +1,17 @@
+data = {"category_component": "Navigation_system","component_selected": "Intel_Realsense_RPLIDAR_A2", "components_spec_data": "Serial", "email": "kornbot380@hotmail.com", "project_name": "Roboreactor_devian_8"}
+data2 = {"category_component": "Navigation_system", "component_selected": "SIM900", "components_spec_data": "UART", "email": "kornbot380@hotmail.com", "project_name": "Roboreactor_devian_8"}
+#Select the component seprately inside the project data 
+project_components = {}
+#append data inside the list  check the email and generate predata insert 
+if data.get('email') not in list(project_components):
+                  project_components[data.get('email')] = {data.get('project_name'):{data.get('category_component'):[{data.get('component_selected'):data.get('components_spec_data')}]}}
+print('Append first datao',project_components)
+if data.get('email') in list(project_components):
+                  
+            if data2.get('project_name') in list(project_components.get(data.get('email'))):   
+                  print(data2)
+                  project_components.get('kornbot380@hotmail.com').get(data2.get('project_name')).get(data2.get('category_component'))[0][data2.get('component_selected')] = data2.get('component_spec_data')  
+
+            if data2.get('project_name') not in list(project_components.get(data.get('email'))):
+                  project_components.get('kornbot380@hotmail.com')[data2.get('project_name')] = {data2.get('project_name'):{data2.get('category_component'):[data2.get('component_selected')]}} 
+print("Result from new input",project_components)
